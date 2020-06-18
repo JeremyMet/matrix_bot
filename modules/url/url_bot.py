2187f483b9409368bbaf750561162e3f8e0bab34
JeremyMet <metairie.jeremy@gmail.com>
2020-06-18 22:06:15 +0200

    Ajout du module url et corrections diverses

---
+++ modules/url/url_bot.py
@@ -0,0 +1,61 @@
+from modules.module import module ;
+import urllib.request
+import urllib.parse
+import unidecode ;
+import re ;
+
+def findTitleOther(url):
+    title = "";
+    try:
+        webpage = urllib.request.urlopen(url, timeout=5).read()
+        webpage = webpage.decode(encoding="utf8")
+        title = webpage.split('<title>')[1].split('</title>')[0]
+        title += '\n';
+    except:
+        pass
+    return title
+
+def findTitleYouTube(url):
+    title = "";
+    try:
+        webpage = urllib.request.urlopen(url, timeout=5).read()
+        webpage = webpage.decode(encoding="utf8")
+        webpage = webpage.split("https://www.youtube.com/watch?v")[1];
+        title = webpage.split("content=\"")[1].split('\"')[0]
+        title += '\n';
+    except:
+        pass
+    return title
+
+
+class url_bot(module):
+
+    url_regex = re.compile("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", re.IGNORECASE)
+
+    def __init__(self, keyword = "url_bot", is_permanent = False): # <- template ... Here goes your default module name
+        super().__init__(keyword, is_permanent) ;
+        self.module_name = "url_bot"
+        self.whatis = "Get small description from url"
+        self.__version__ = "0.0.1"
+        self.help = "None"
+
+    def process_msg_passive(self, cmd, sender, room):
+        reg_match = re.finditer(url_bot.url_regex, cmd);
+        ret = "" ;
+        for match in reg_match: # si une url valide est trouvée ...
+            url = match.group(0) ;
+            print(">>> current url: {}".format(url))
+            if url.find("youtube.") > 0:
+                ret += findTitleYouTube(url);
+            elif url.find("twitter.") > 0:
+                pass
+            else:
+                ret+= findTitleOther(url);
+        return ret;
+
+if __name__ == "__main__":
+    url = "https://www.youtube.com/watch?v=KXatvzWAzLU";
+    title = findTitleYouTube(url)
+    parsed_url = urllib.parse.urlparse(url);
+    print(title)
+    print(parsed_url.netloc)
--- server.py
+++ server.py
@@ -7,6 +7,7 @@ from modules.quotes.quotes import quotes ;
 from modules.template.template import template ;
 from modules.regex.regex import regex ;
 from modules.loto.loto_bot import loto_bot;
+from modules.url.url_bot import url_bot;
 import time ;
 
 
@@ -18,17 +19,22 @@ if __name__ == "__main__":
     my_mastermind = mastermind_bot("mastermind", is_permanent = True)
     #my_greeting = greeting("greeting") ;
     #my_admin = admin("admin", is_permanent = True) ;
-    my_quotes = quotes("quotes", hour=23, minute=0) ;
+    my_quotes = quotes("quotes", hour=7, minute=0) ;
     my_loto = loto_bot("loto", hour=20, minute=30);
+    my_url = url_bot();
     # Then Create the matrix object, add rooms, services and timers.
     matrix_obj = matrix_utils_ext() ;
     gaming_room = matrix_obj.add_room("#botgaming:mandragot.org")
-    # gaming_room = matrix_obj.add_room("#admin:mandragot.org")
+    science_room = matrix_obj.add_room("#sciences:mandragot.org")
+
 
     matrix_obj.add_service_to_room(gaming_room, my_quotes) ;
     matrix_obj.add_service_to_room(gaming_room, my_pendu) ;
     matrix_obj.add_service_to_room(gaming_room, my_mastermind) ;
     matrix_obj.add_service_to_room(gaming_room, my_loto) ;
+    matrix_obj.add_service_to_room(gaming_room, my_url) ;
+
+    matrix_obj.add_service_to_room(science_room, my_url) ;
 
     matrix_obj.start_timer() ; # start clock thread (for clock sensitive processes/modules)
     matrix_obj.spawn() ;
