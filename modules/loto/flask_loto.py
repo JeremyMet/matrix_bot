fee6ff27ce430db08017a8a69cce3a5f17c21c4c
root <metairie.jeremy@gmail.com>
2021-03-16 20:44:48 +0100

    nouveau mécanisme LOTO

--- modules/loto/flask_loto.py
+++ modules/loto/flask_loto.py
@@ -13,15 +13,16 @@ def index():
 	f_normalize = lambda x : x.lower().capitalize();
 	score_dic = {};
 	try:
-		with open("./modules/loto/scoreboard_file.dic", "rb") as picke_file:
+		with open("scoreboard_file.dic", "rb") as picke_file:
 			score_dic = pickle.load(pickle_file);
 	except:
 		pass
 	score_array = [];
 	for key_value, value in sorted(score_dic.items(), key=lambda x: x[1], reverse=True):
 		score_array.append((f_normalize(key_value), value));
-	current_month = MONTHS[datetime.datetime.now().month];
+	current_month = MONTHS[datetime.datetime.now().month-1];
 	return render_template("index.html", length=len(score_array), score_array=score_array, current_month=current_month)
+	print(score_array)
 
 if __name__ == '__main__':
-	app.run()
+	app.run(host="0.0.0.0");
